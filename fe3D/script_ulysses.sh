8c4e70ab09bc3842513fae43568dedc9470e1e1d
denocris <denobili16@gmail.com>
2017-03-04 15:25:06 +0100

    minor mod

--- fe3D/script_ulysses.sh
+++ fe3D/script_ulysses.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+#PBS -l nodes=1:ppn=20
 #PBS -l walltime=10:00:00
 
 cd $PBS_O_WORKDIR
 
-python fe3D_p12.py 
+python fe3D_p12.py
--- fe3D/script_ulysses_mfree.sh
+++ fe3D/script_ulysses_mfree.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#PBS -l nodes=1:ppn=20
 #PBS -l walltime=10:00:00
 
 cd $PBS_O_WORKDIR
